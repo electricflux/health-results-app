a8198f23127fb7345bfa0e34152e39b7ad2b1a9f
Sid <electricflux@gmail.com>
2018-04-27 16:29:07 -0700

    1. Add react-chartjs support 2. Add webpack-dev-server support

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,29 @@
 // This library allows us to combine paths easily
 const path = require('path');
+var debug = process.env.NODE_ENV !== "production";
 module.exports = {
-   entry: path.resolve(__dirname, 'react', 'index.jsx'),
-   output: {
-      path: path.resolve(__dirname, 'output'),
-      filename: 'bundle.js'
-   },
-   resolve: {
-      extensions: ['.js', '.jsx']
-   },
-   module: {
-      rules: [
-         {
-             test: /\.jsx/,
-             use: {
-                loader: 'babel-loader',
-                options: { presets: ['react', 'es2015'] }
-             }
-         },
-         {
-            test: /\.scss/,
-            use: ['style-loader', 'css-loader', 'sass-loader']
-         }
-      ]
-   }
+    entry: path.resolve(__dirname, 'react', 'index.jsx'),
+    output: {
+        path: path.resolve(__dirname, 'output'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: { presets: ['react', 'es2015'] }
+                }
+            },
+        ]
+    },
+    plugins: debug ? [] : [
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.OccurrenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+    ]
 };
